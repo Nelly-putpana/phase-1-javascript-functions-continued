0893df9aa21d27c3b28203b305ba6ca129d9c8a5
Nelly Putpana <nelly.putpana@gmail.com>
2023-04-09 16:28:40 -0600

    functiones labcontinued

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+//function declaration
+function saturdayFun(activity = "roller-skate") {
+    const firstParameter = "dance";
+    if (firstParameter === "dance")
+    return `This Saturday, I want to ${activity}!`;
+}
+
+//function expression
+
+const mondayWork = function (activity = "go to the office"){
+    const myFirstParameter = "work from home.";
+    if (myFirstParameter === "work from home.")
+    return `This Monday, I will ${activity}.`;
+
+}
+
+// function 
+function  wrapAdjective (mydefault = "*") {
+    return function (message = "special") {
+        return `You are ${mydefault}${message}${mydefault}!`
+    };
+}
+wrapAdjective("||")("a dedicated programmer");
